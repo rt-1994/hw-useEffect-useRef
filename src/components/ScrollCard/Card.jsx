edd18c78ca30830d838cb4173a21ec2ec78cb839
Rustem <sernazarovrustem@gmail.com>
2023-02-07 23:04:32 +0500

    Third

--- src/components/ScrollCard/Card.jsx
+++ src/components/ScrollCard/Card.jsx
@@ -2,23 +2,19 @@ import React, {useState, useEffect, useRef} from "react";
 import styles from "./Card.scss";
 import cn from "classnames";
 
-
 const Card = () => {
     const [value, setValue] = useState(0)
     const circleRef = useRef(null)
 
-
-
     useEffect(() => {
         const bar = circleRef.current;
         const style = getComputedStyle(bar).strokeDasharray;
-        const rangeRE = /^([1-9]?\d|100)$/;
-        if (rangeRE.test(value)) {
+        const range = /^([1-9]?\d|100)$/;
+        if (range.test(value)) {
             let styleNew = parseFloat(style) * (1 - parseFloat(value / 100));
             bar.style.strokeDashoffset = styleNew;
         }
     },[value])
-
 
     return (
         <div className={styles.content}>
@@ -31,10 +27,14 @@ const Card = () => {
                         <p>#</p>
                         <p>!</p>
                     </div>
-
                     <div className={styles.statusBar}>
                         <svg className={styles.progress}>
-                            <circle className={styles.bar} r="47.5%" cx="50%" cy="50%"></circle>
+                            <circle
+                                className={styles.bar}
+                                r="47.5%"
+                                cx="50%"
+                                cy="50%"
+                            ></circle>
 
                             <circle
                                 className={cn(styles.bar, styles.progress__circle)}
@@ -44,12 +44,10 @@ const Card = () => {
                                 cy="50%"
                             ></circle>
                         </svg>
-
                         <p className={styles.procent}>{value} %</p>
                     </div>
                 </div>
             </div>
-
         </div>
     )
 }
